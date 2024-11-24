0e04a4d6ed3615a118f3f025341c5da669c91078
Muntu12 <muntazir@eng.rizvi.edu.in>
2024-11-24 16:50:42 +0530

    Add files via upload

---
+++ Experiment8/exp8task2muntu.c
@@ -0,0 +1,27 @@
+/*Aim : WAP to check if the entered string is pallindrome or not.
+Name : Muntazir Patel
+Roll no : 12
+UIN : 241A012
+Division : A*/
+#include<stdio.h>
+int main()
+{
+    char str[100]; //character string of size 100
+    int i;
+    printf("\t\t\t*** Pallindrome Finder ***\n\n\n");
+
+    printf("\n Enter Your String : "); //Input String from user
+    gets(str);
+
+for(i = 0; i < strlen(str) - 1; i++)
+    {
+        if(str[i] != str[strlen(str) - 1 - i])
+        {
+            printf("The String is NOT a Palindrome");
+            return 0;
+        }
+    }
+ printf("\n The String is Pallindrome.");
+
+ return 0;
+}
